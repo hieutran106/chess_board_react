4b48dc71fd9532e81bcb86b81f8342ccb6c01e89
Hieu Tran <hieutran106@gmail.com>
2016-12-29 13:48:46 +1000

    add draggable

--- src/ChessPiece.js
+++ src/ChessPiece.js
@@ -2,20 +2,29 @@
  * Created by hieutran on 12/29/2016.
  */
 import React from 'react'
+import Draggable from 'react-draggable'
 import './App.css'
+
 
 var ChessPiece = React.createClass({
 
     componentWillMount() {
-        console.log(this.props.pos);
         this.style = {
-             left: this.props.pos.j * 90 + 'px',
-             top: this.props.pos.i * 90 + 'px'
+            left: this.props.pos.j * 90 + 'px',
+            top: this.props.pos.i * 90 + 'px'
 
         }
     },
+    handleStop(e) {
+        console.log(e);
+
+    },
     render() {
-        return (<div className="chess_pieces" style={this.style}><img src={"img/"+this.props.type+".png"} role="presentation"/></div>);
+        return (<Draggable onStop={this.handleStop} grid={[90, 90]}>
+                <div className="chess_pieces" style={this.style}><img src={"img/" + this.props.type + ".png"}
+                                                                      role="presentation"/></div>
+            </Draggable>
+        );
 
     }
 })
